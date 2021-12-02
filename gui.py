e55e7d2efb00d7ff865376eca26108faf53cc6ee
erika-hortencia <erika.cardoso@usp.br>
2021-12-02 15:45:34 -0300

    Limpeza de código em appInsertion.py

--- gui.py
+++ gui.py
@@ -1,26 +1,43 @@
 import cx_Oracle
 import sqlConnection
+import appInsertion
 from tkinter import *
-
-#connection = cx_Oracle.connect('oe', 'oracle', 'myservice')
 
 connection = sqlConnection.connect()
 
 
 def insertion():
+    """
+    Criando a janela de inserção
+    """
+    insertWindow = Tk()
+    insertWindow.title('Inserção de Dados')
+    insertion_text = Label(insertWindow, text='Clique em Ok  para prosseguir')
+    insertion_text.grid(column=0, row=0, padx= 20, pady= 15)
+
+    okButton = Button(insertWindow, text='Ok')
+    okButton.grid(column=0, row=1, padx= 60, pady= 45)
+
     selectedOptionText['text'] ='Inserindo'
     #print('Inserindo')
     """
     TESTE DE INSERÇÃO
     """
-    cur = connection.cursor()
-    sql_insert = """INSERT INTO TMORADORDERUA VALUES(999991293,'JOHN DOE',TO_DATE('28/04/2003','dd/mm/yyyy'),TO_DATE('30/11/2015','dd/mm/yyyy'),'M', 'MEDIO')"""
-    cur.execute(sql_insert)
-    connection.commit()
-    cur.execute('select * from tMoradorDeRua')
-    for line in cur:
-        print(line)
-    cur.close()
+    try:
+        # Criando o cursor
+        cur = connection.cursor()
+        sql_insert = """INSERT INTO TMORADORDERUA VALUES(912341391,'JOHN STAMOS',TO_DATE('28/04/2003','dd/mm/yyyy'),TO_DATE('30/11/2015','dd/mm/yyyy'),'M', 'MEDIO')"""
+        cur.execute(sql_insert)
+        connection.commit()
+    except Exception as err:
+        print('Erro ao inserir os dados')
+    else:
+        cur.execute('select * from tMoradorDeRua')
+        for line in cur:
+            print(line)
+    finally:
+        cur.close()
+        connection.close()
     
 
 def search():
@@ -50,4 +67,6 @@ selectedOptionText = Label(homeWindow, text=" ")
 selectedOptionText.grid(column=0, row=4, padx= 60, pady= 45)
 
 homeWindow.mainloop()
-connection.close()
+
+# Encerra a conexão
+#connection.close()
